43ad6d402af958de80b5b65a2215b5903a34dd0f
gayusra <dhana.gayathri100@gmail.com>
2025-12-05 12:24:01 +0530

    new file added

--- math.js
+++ math.js
@@ -36,3 +36,53 @@ for(let i = num.length-1; i>=0;i--){
     reversed.push(num[i])
 }
 console.log(reversed)
+
+//Generate a random 4-digit OTP using Math.random.
+//Standard 4-digit OTP (1000 to 9999)
+function generateOTP() {
+  return Math.floor(1000 + Math.random() * 9000);
+}
+
+console.log(generateOTP());
+
+//How it works:
+/* Math.random() → gives a value between 0 and 1
+
+Math.random() * 9000 → gives a value between 0 and 9000
+
+1000 + ... → shifts it to 1000–9999 range
+
+Math.floor() → removes decimals */
+
+
+//Random Password Generator
+function generatepassword(length){
+      const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$&!";
+      let password = ""
+
+      for(let i = 0; i<length; i++){
+        let randomIndex = Math.floor(Math.random()* characters.length)
+        password += characters[randomIndex]
+      }
+      return password
+}
+
+console.log(generatepassword(10))
+
+//how its work
+
+/* Math.random() generates a random number between 0 and 1
+
+Example: 0.55672…
+
+Multiply by characters.length
+
+Example:
+0.55672 × 68 = 37.854 → random index
+
+Math.floor() converts it to whole number
+
+37 → character at index 37
+
+Loop repeats until the password reaches your required length.
+*/
